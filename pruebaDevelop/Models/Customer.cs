6fd55210edeb5212fac9c08a09d6f7873780904e
Luis Batres <87052175+LuisBatres@users.noreply.github.com>
2024-10-03 11:00:37 -0600

    Add files via upload

---
+++ pruebaDevelop/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace pruebaDevelop.Models
+{
+    public class Customer
+    {
+        // customerId, Email, Teléfono, Direcciones, Fecha de cumpleaños, Nombre y Apellido.
+
+        [JsonProperty("customerId")]
+        public string? CustomerId { get; set; }
+
+        [JsonProperty("email")]
+        public string? Email { get; set; }
+
+        //[JsonProperty("tel")]
+        //public string? Phone { get; set; }
+
+        [JsonProperty("birthday")]
+        public string? Birthday { get; set; }
+
+        [JsonProperty("firstName")]
+        public string? FirstName { get; set; }
+
+        [JsonProperty("lastName")]
+        public string? LastName { get; set; }
+    }
+}
