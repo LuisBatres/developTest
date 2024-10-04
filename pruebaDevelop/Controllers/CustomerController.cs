37ea205f17da464747748de184936947a4bf40e7
Luis Batres <alu.20130806@correo.itlalaguna.edu.mx>
2024-10-04 13:39:02 -0600

    search by postal code

--- pruebaDevelop/Controllers/CustomerController.cs
+++ pruebaDevelop/Controllers/CustomerController.cs
@@ -3,8 +3,6 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using pruebaDevelop.Models;
 using RestSharp;
-using System.Reflection;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace pruebaDevelop.Controllers
 {
@@ -12,7 +10,8 @@ namespace pruebaDevelop.Controllers
     [ApiController]
     public class CustomerController : ControllerBase
     {
-        // GET: api/<ValuesController>
+        // Servicio para obtener solo la información importante del cliente como:
+        // customerId, Email, Teléfono, Direcciones, Fecha de cumpleaños, Nombre y Apellido.
         [HttpGet]
         [Route("CustomerInformation")]
         public async Task<Customer> GetCustomerInfo()
@@ -45,22 +44,20 @@ namespace pruebaDevelop.Controllers
             Customer? customerResponse = JsonConvert.DeserializeObject<Customer>(json.ToString());
             //ya tenemos respuesta
 
-            if (customerResponse == null) 
+            if (customerResponse == null)
                 return new Customer();
-            
+
             return customerResponse;
         }
 
-        [HttpGet]
-        [Route("SortAddresses")]
-        public async Task<Customer> SortAddressesBy([FromQuery] string nombre)
-        {
 
-        }
+        // Servicio que retorne la dirección preferida del cliente.
+        // (En base a la propiedad “preferred” de la lista “address”).
 
+        // Servicio para buscar direcciones del cliente por código postal.
         [HttpGet]
-        [Route("PreferedAddress")]
-        public async Task<Customer> PreferedAddress([FromQuery] string customer)
+        [Route("SearchAddressByPostalCode")]
+        public async Task<Address> SearchAddress([FromQuery] string postalCode)
         {
             RestClient restClient = new RestClient();
             RestRequest restRequest = new RestRequest()
@@ -75,7 +72,7 @@ namespace pruebaDevelop.Controllers
             //text
 
             if (restResponse.Content == null)
-                return new Customer();
+                return new Address();
 
             //se deserializa la respuesta  
             string? desResponse = JsonConvert.DeserializeObject(restResponse.Content).ToString();
@@ -84,16 +81,16 @@ namespace pruebaDevelop.Controllers
             JObject json = JObject.Parse((string)token);
 
             if (json == null)
-                return new Customer();
+                return new Address();
 
-            //tenemos el JSON y convertimos en objeto de utilidad
-            Address? addressResponse = JsonConvert.DeserializeObject<Address>(json.ToString());
-            //ya tenemos respuesta
+            string? address = json["addresses"].Where(a => a[postalCode] == a["postalCode"]).Select(a => a["address1"].ToString()).FirstOrDefault();
+
+            Address? addressResponse = JsonConvert.DeserializeObject<Address>(address.ToString());
 
             if (addressResponse == null)
-                return new Customer();
+                return new Address();
 
-
-            //return customerResponse;
+            return addressResponse;
         }
     }
+}
